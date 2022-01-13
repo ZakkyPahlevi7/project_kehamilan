54e5fec9eaf843b998bb812784e99dda092ed944
ZakkyPahlevi7 <gitzakkypahlevi@gmail.com>
2022-01-13 15:22:55 +0700

    90 persen project

--- lib/main.dart
+++ lib/main.dart
@@ -32,50 +32,62 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(
-          Icons.arrow_back,
-          color: Colors.black,
+        appBar: AppBar(
+          leading: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          title: Text(
+            title,
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.white,
         ),
-        title: Text(
-          title,
-          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-        ),
-        backgroundColor: Colors.white,
-      ),
-      body: Container(
-        child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.fromLTRB(0, 20, 125, 10),
-              child: Text('Hari Pertama Haid Terakhir (HPHT)', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
-            ),
-           Container(
-              margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
-              child: TextField(
-              decoration: InputDecoration(border: OutlineInputBorder(), hintText: 'dd/mm/yyyy'),
-            )
-           ),
-            Container(
-              margin: EdgeInsets.fromLTRB(0, 20, 210, 10),
-              child: Text('Lama siklus haid rata rata', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
-            ),
-           Container(
-              margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
-              child: TextField(
-              decoration: InputDecoration(border: OutlineInputBorder(), hintText: 'Masukkan jumlah hari'),
-            )
-           ),
-           Container(
-             margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
-             child: RaisedButton(onPressed: (){},
-             color: Color(0xff2D9CDB),
-             child: Text('Hitung', style: TextStyle(color: Colors.white),)
-             ),
-           )
-          ],
-        ),
-      )
-    );
+        body: Container(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 20, 125, 10),
+                child: Text(
+                  'Hari Pertama Haid Terakhir (HPHT)',
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'dd/mm/yyyy',
+                        suffixIcon: Icon(Icons.date_range_outlined)),
+                  )),
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 20, 210, 10),
+                child: Text(
+                  'Lama siklus haid rata rata',
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Masukkan jumlah hari',
+                    ),
+                  )),
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.fromLTRB(195, 25, 195, 25)
+                  ),
+                  onPressed: () {},
+                  child: const Text('Hitung', style: TextStyle(fontSize: 17),),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
